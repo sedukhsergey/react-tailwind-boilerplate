27c34412c66990114df4bfb5123456c76d3f1ab0
sedukh <sedukhsergey@gmail.com>
2020-01-02 21:49:11 +0200

    first commit. Base structure

---
+++ src/containers/Header/Header.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import blueLogo from '../../images/react.svg';
+import redLogo from '../../images/redLogo.svg';
+
+const Header = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    return (
+        <div className='
+            md:flex md:flex-row md:justify-between
+            bg- p-3 bg-white rounded-sm mb-8 bg-blue-300 shadow-lg
+        '>
+            <div className='md:flex'>
+                <h2>Header company name</h2>
+                <img src={isOpen ? blueLogo : redLogo} alt="logo" className='
+                md:ml-2 md:w-6
+                w-4 rounded-lg'/>
+            </div>
+            <button onClick={() => setIsOpen(state => !state)}>{isOpen ? 'logout' : 'login'}</button>
+        </div>
+    )
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './containers/App/App';
 import * as serviceWorker from './serviceWorker';
-
+import './styles/tailwind.css';
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
