ec1611baae6cefd2fe2d8fb563934922b5be781b
sedukh <sedukhsergey@gmail.com>
2020-01-07 00:23:53 +0200

    refactoring;

--- src/components/HorizontalLine/HorizontalLine.tsx
+++ src/components/HorizontalLine/HorizontalLine.tsx
@@ -1,22 +1,17 @@
-import React from "react";
-import styles from './HorizontalLine.styles'
-import { Styles } from "../../constants";
-import { useStyles } from "../../hooks";
+import React from 'react';
+import styles from './HorizontalLine.styles';
+import { Styles } from '../../constants';
+import { useStyles } from '../../hooks';
 
 interface Props extends Styles {}
-const HorizontalLine: React.FC<Props> = ({
-    looks = '',
-    customStyles,
-}) => {
-    const [classNames] = useStyles({looks, styles});
-    return (
-        <hr className={classNames} style={customStyles}/>
-    )
-}
+const HorizontalLine: React.FC<Props> = ({ looks = '', customStyles }) => {
+    const [classNames] = useStyles({ looks, styles });
+    return <hr className={classNames} style={customStyles} />;
+};
 
 HorizontalLine.defaultProps = {
     customStyles: {},
-    looks: 'default',
-}
+    looks: 'default'
+};
 
 export default React.memo(HorizontalLine);
